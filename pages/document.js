f7598999a1c069648c4c881d7458ddd4c683a821
Peter Cummuskey <peterc@aetheric.co.nz>
2014-04-14 11:08:27 +0000

    Added file-based document access.

--- pages/document.js
+++ pages/document.js
@@ -1,8 +1,9 @@
 module.exports = function(express) {
+	var fs = require('fs');
 
 	express.get('/document/:docId', function(req, res) {
-
-		var document = null;
+		var docFile = __dirname + '/data/documents/' + req.params.docId + '.json';
+		var document = fs.readFileSync(docFile);;
 
 		res.render('document', {
 			title: 'Document View',
@@ -10,4 +11,4 @@ module.exports = function(express) {
 		});
 	});
 
-});
+};
