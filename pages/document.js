0cbb4297d6a0d9e32c53424ca2c22adaa4efed8e
Peter Cummuskey <peterc@aetheric.co.nz>
2014-04-14 20:17:49 +0000

    Fixing path issue with document loading.

--- pages/document.js
+++ pages/document.js
@@ -2,7 +2,9 @@ module.exports = function(express) {
 	var fs = require('fs');
 
 	express.get('/document/:docId', function(req, res) {
-		var docFile = __dirname + '/data/documents/' + req.params.docId + '.json';
+		var docFile = __dirname + '/../data/documents/' + req.params.docId + '.json';
+		
+		//TODO: Check that file exists.
 		var document = fs.readFileSync(docFile);;
 
 		res.render('document', {
