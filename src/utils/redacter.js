7b290e02531dfb167e821276121338a16b9b86c2
Peter Cummuskey <peterc@aetheric.co.nz>
2014-04-19 03:55:01 +0000

    Partial work on redaction code.

--- src/utils/redacter.js
+++ src/utils/redacter.js
@@ -3,7 +3,14 @@ module.exports = function(data) {
 	function redact(document) {
 		var redacted = document.content;
 
+		// Paragraphs
+		redacted = redacted.replace(/\n([.\n]+?)\n\n/g, '\1');
+
+		// Manual line-breaks
 		redacted = redacted.replace(/\n/g, '<br/>');
+
+		// Redaction
+		redacted = redacted.replace(/\[.+?\]([.\n]*?)\w([.\n]*?)\[\/\]/g, '\1*\2');
 
 		return redacted;
 	}
