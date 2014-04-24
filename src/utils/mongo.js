0080e0e6c1feb5a3ab5ca5dcbf77dde8c702d780
pcummuskey <pcummuskey@bravurasolutions.com>
2014-04-24 12:43:25 +1200

    Fixed some errors and added some actual functionality.

--- src/utils/mongo.js
+++ src/utils/mongo.js
@@ -1,8 +1,22 @@
-module.exports(data) {
+module.exports = function(data) {
 	var mongo = require('mongodb').MongoClient;
+	var format = require('format');
 
-	mongo.connect('mongodb://127.0.0.1:27017/intelligencia', function(err, db) {
-		if(err) throw err;
+	var db = {
+		user: process.env.DB_USER || 'intelligencia',
+		pass: process.env.DB_PASS || 'notpassword',
+		host: process.env.DB_HOST || 'oceanic.mongohq.com',
+		port: process.env.DB_PORT || '10096',
+		path: process.env.DB_PATH || 'app24119285'
+	};
+
+	var dburi = format('mongodb://%s:%s@%s:%d/%s', db.user, db.pass, db.host, db.port, db.path);
+
+	return data.fnMongo = function(callback) {
+		mongo.connect(dburi, function(err, db) {
+			if(err) throw err;
+			callback(db);
+		});
 	}
 
-}
+};
