c84b77f5c1b99a37d3d8892d4a842a30317bb919
Peter Cummuskey <peterc@aetheric.co.nz>
2014-05-17 04:23:57 +0000

    Threw some login code up in this bitch.

--- src/pages/auth/auth_login.js
+++ src/pages/auth/auth_login.js
@@ -22,8 +22,44 @@ module.exports = function(express, data, page) {
 		});
 	});
 
+	var invalidmsg = {
+		type: 'error',
+		text: 'No user exists with that name and password.'
+	};
+
 	express.post(page.path, function(req, res) {
-		// TODO: Check login credentials.
+		var username = req.body.username;
+		var password = req.body.password;
+
+		if (!username || !password) {
+			res.flash.username = username;
+			res.flash.message = {
+				type: 'error',
+				text: 'Aren\'t you forgetting something?'
+			};
+			
+			res.redirect(page.path);
+			return;
+		}
+
+		data.fnMongo(function(db) {
+			db.collection('users').find({ username: username }).nextObject(function(err, user) {
+				if (err) throw err;
+
+				var hash = password;
+
+				if (!user || user.password !== hash) {
+					res.flash.username = username;
+					res.flash.message = invalidmsg;
+					res.redirect(page.path);
+					return;
+				}
+
+				req.session.user = user;
+				res.redirect(data.pages.user_dash.path);
+			});
+		});
+
 	});
 
 };
