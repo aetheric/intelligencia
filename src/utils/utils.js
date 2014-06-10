3518358b88019979a78def76941dfa569b06b767
pcummuskey <pcummuskey@bravurasolutions.com>
2014-06-11 11:21:10 +1200

    Made server error message logging and display more comprehensive.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 module.exports = function (data) {
+	var _ = require('underscore');
 
-	String.prototype.stripSuffix = function (suffix, startFrom) {
+	String.prototype.stripSuffix = function(suffix, startFrom) {
 		var start = 0;
 		if (startFrom) {
 			var idx = this.indexOf(startFrom);
@@ -12,13 +13,21 @@ module.exports = function (data) {
 		return this.substring(start, this.lastIndexOf(suffix));
 	};
 
-	String.prototype.remove = function (regex) {
+	String.prototype.remove = function(regex) {
 		return this.replace(regex, '');
+	};
+
+	var formatRegex = /\{\}/;
+	String.prototype.format = function() {
+		return _.reduce(arguments, function(memo, argument) {
+			return memo.replace(formatRegex, argument);
+		}, this);
 	};
 
 	/**
 	 * This function allows for single-line handling of critical error conditions. Should be used in code as follows:
-	 * <code>if (data.fnHandleError(res, err)) return;</code>.
+	 * <code>if (data.fnHandleError(res, err)) return;</code>. Will log an error stacktrace, and format a message for
+	 * the user, as well as redirecting the current response to the server error page.
 	 * @param res The response object for the current request.
 	 * @param error The error that may or may not have been thrown.
 	 * @returns {boolean} If an error was thrown, true. Otherwise false.
@@ -29,8 +38,12 @@ module.exports = function (data) {
 		// log the error to the console.
 		console.error(error.stack);
 
+		// Create a formatted error message.
+		var message = error.code ? ' ({})'.format(error.code) : '';
+		message = '{}{}: {}'.format(error.name, message, error.message);
+
 		// Redirect to server error page with message.
-		res.flash.message('error', error.name + ' (' + error.code +  '): ' + error.message);
+		res.flash.message('error', message);
 		res.redirect(data.pages.error_server.path);
 
 		return true;
