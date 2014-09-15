dbd0adf8a982f962a1be20c4bedabef7b1a23c1b
pcummuskey <pcummuskey@bravurasolutions.com>
2014-09-15 16:57:06 +1200

    Adding code for retrieving and displaying users in the admin interface.

--- src/pages/admin/user/user_list.js
+++ src/pages/admin/user/user_list.js
@@ -1,8 +1,17 @@
 module.exports = function(express, data, page) {
 
 	express.get(page.path, function(req, res) {
-		res.render(page.template, {
-			title: 'User List'
+		data.fnMongo(function(err, db) {
+			if (data.fnHandleError(res, err)) return;
+
+			db.collection('docs').find().toArray(function(err, items) {
+				if (data.fnHandleError(res, err)) return;
+
+				res.render(page.template, {
+					title: 'User List',
+					users: items
+				});
+			});
 		});
 	});
 
