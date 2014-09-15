34b65d7176e60b3a95e5522f431b744cfba25dcf
pcummuskey <pcummuskey@bravurasolutions.com>
2014-09-15 17:03:48 +1200

    Added a crappy user display to the admin area.

--- src/pages/admin/user/user_view.js
+++ src/pages/admin/user/user_view.js
@@ -1,8 +1,21 @@
 module.exports = function(express, data, page) {
 
 	express.get(page.path + '/:userId', function(req, res) {
-		res.render(page.template, {
-			title: 'User Detail'
+		var userId = data.fnMongoId(req.params.userId);
+
+		data.fnMongo(function(err, db) {
+			if (data.fnHandleError(res, err)) return;
+
+			db.collection('users').find({ _id: userId }).nextObject(function(err, user) {
+				if (data.fnHandleError(res, err)) return;
+
+				//TODO: Handle missing user.
+
+				res.render(page.template, {
+					title: 'User Detail',
+					user: user
+				});
+			});
 		});
 	});
 
