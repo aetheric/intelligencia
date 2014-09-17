393096dcb3e1c243a5367045181e24a21e7f3b14
pcummuskey <pcummuskey@bravurasolutions.com>
2014-09-17 17:54:33 +1200

    Fiddled more with the data tests.

--- src/test/service/testData.js
+++ src/test/service/testData.js
@@ -2,13 +2,16 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var proxyquire = require('proxyquire');
 var _ = require('underscore');
+var mongo = require('mongodb');
 
 describe('the data service', function() {
 	var data;
 	var client;
+	var expectedCalls;
 
 	beforeEach(function() {
-		data = proxyquire('../main/service/data', {
+		expectedCalls = 0;
+		data = proxyquire('../../main/service/data', {
 			mongo: {
 				client: client = {}
 			}
@@ -30,12 +33,18 @@ describe('the data service', function() {
 		it('should be a function', function() {
 			expect(data).to.have.property('init');
 			expect(data.init).to.be.a('function');
-			expect(data.init({})).to.be.a('Promise');
+		});
+
+		it('should return a promise', function() {
+			var output = data.init({});
+			expect(output).to.be.an('object');
+			expect(output).to.have.property('then');
 		});
 
 		it('should connect to the database', function() {
-			data.init({});
-			sinon.assert.calledOnce(client.connect);
+			data.init({}).then(function() {
+				sinon.assert.calledOnce(client.connect);
+			});
 		});
 
 	});
@@ -45,12 +54,20 @@ describe('the data service', function() {
 		it('should be a function', function() {
 			expect(data).to.have.property('close');
 			expect(data.close).to.be.a('function');
-			expect(data.close()).to.be.a('Promise');
+		});
+
+		it('should return a promise', function() {
+			var output = data.close();
+			expect(output).to.be.an('object');
+			expect(output).to.have.property('then');
 		});
 
 		it('should close the current connection', function() {
-			data.close();
-			sinon.assert.calledOnce(client.close);
+			data.init({}).then(function() {
+				data.close().then(function() {
+					sinon.assert.calledOnce(client.close);
+				});
+			});
 		});
 
 	});
@@ -60,12 +77,37 @@ describe('the data service', function() {
 		it('should be a function', function() {
 			expect(data).to.have.property('getDocumentDetail');
 			expect(data.getDocumentDetail).to.be.a('function');
-			expect(data.getDocumentDetail(0)).to.be.a('Promise');
+		});
+
+		it('should return a promise', function() {
+			var output = data.getDocumentDetail(0);
+			expect(output).to.be.an('object');
+			expect(output).to.have.property('then');
 		});
 
 		it('should access the document collection', function() {
 			data.getDocumentDetail(0).then(function(document, error) {
 				sinon.assert.calledOnceWith(client.db.collection, 'docs');
+			});
+		});
+
+		it('should find by the mongo-fied object id', function() {
+			var documentId = 12;
+			var mockCollection = {
+				find: sinon.spy()
+			};
+
+			data.init({}).then(function(connection) {
+
+				connection.collection = function() {
+					return mockCollection;
+				};
+
+				data.getDocumentDetail(documentId).then(function() {
+					sinon.assert.calledOnceWith(mockCollection.find, {
+						_id: new mongo.ObjectId(documentId)
+					});
+				});
 			});
 		});
 
